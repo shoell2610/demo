89fb983c76e2bb24a96e1ee1e50943bcc2b4325f
shoell <shoell@smail.uni-koeln.de>
2023-01-13 11:43:14 +0100

    Initial commit

---
+++ src/main/java/com/HW12/springboot/Hw12Application.java
@@ -0,0 +1,13 @@
+package com.HW12.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw12Application.class, args);
+	}
+
+}
